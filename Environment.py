f911f73b3351a1c1ef3710390ae87ad391388b0b
Tarek Sallam <tarek.i.sallam@gmail.com>
2024-07-10 22:11:23 -0700

    added get top projected method of the draft board class

--- Environment.py
+++ Environment.py
@@ -6,18 +6,40 @@ import random
 
 class DraftBoard():
     def __init__(self, teams: int, agentPick: int, data_path: str):
+        
+        # set the agent pick and the amount of teams attributes
         self.agentPick = agentPick;
         self.teams = teams;
         players = pd.read_csv(data_path)
-        self.players = np.array()
-        self.agentPoints = 0;
+        
+        # split the data up into position and make sure they are sorted by projected points
+        rb = players[players["position"] == 'RB'].sort_values('proj', ascending=False)
+        qb = players[players["position"] == 'QB'].sort_values('proj', ascending=False)
+        wr = players[players["position"] == 'WR'].sort_values('proj', ascending=False)
+        k = players[players["position"] == 'K'].sort_values('proj', ascending=False)
+        defs = players[players["position"] == 'DEF'].sort_values('proj', ascending=False)
+        te = players[players["position"] == 'TE'].sort_values('proj', ascending=False)
+
+        # set the players array with the data
+        self.players = [qb, rb, wr, te, k, defs]
+
+    def getTopProjections(self) -> list[float]:
+        l = []
+        for pos in self.players:
+            l.append(pos['proj'].max)
+        return l
+
+    def toNextPick():
+        pass
+
+        
 
 class DraftEnv(Env):
     def __init__(self, teams):
         self.action_space = Discrete(6)
         self.observation_space = Tuple(Discrete(19), Box(low=np.zeros(19), high=np.array(np.ones(19) * np.inf)))
         self.draftBoard = DraftBoard(teams, random.randint(1, teams))
-        self.state = (np.zeros(19), )
+        self.state = (np.zeros(19), np.array(self.draftBoard.getTopProjections()))
         #randomize the agents place in the draft
         #draft all players until the agents pick
     def step (self):
