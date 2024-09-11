fixed bug in the gradient clipping, modified reward function to encourage filling roster

Agent.py
@@ -61,11 +61,12 @@ class PolicyGradientAgent():
         self.epsilon = epsilon
 
     ## learn function that updates the neural network given it's memory
-    def learn(self, baseline, rounds):
+    def learn(self, baseline):
         
         ## get the reward memory
         rewards = self.reward_memory 
         returns = [] ## discounted returns
+        total_loss = 0
         G = 0
 
         ## for each reward (starting from the last reward)
@@ -75,11 +76,11 @@ class PolicyGradientAgent():
             G = reward + self.discount_factor * G
             returns.insert(0, G)
         returns = np.array(returns) ## convert to np.array
-
-        returns -= (baseline / rounds) # baseline the rewards
         
+        returns -= baseline ## baseline the returns
+
         ## whiten the returns with standardization
-       # returns = (returns - np.mean(returns))/(np.std(returns) + 1e-8)
+        #returns = (returns - np.mean(returns))/(np.std(returns) + 1e-8)
 
         ## loop through the states, actions and returns
         for state, action, return_t in zip(self.state_memory, self.action_memory, returns):
@@ -97,21 +98,28 @@ class PolicyGradientAgent():
                 probs = self.policy(state)
                 probs = tf.clip_by_value(probs, 1e-10, 1.0)
                 log_prob = tf.math.log(probs[0, action])
-                loss = -log_prob * return_t # 
+                advantage = return_t - baseline
+                loss = -log_prob * advantage # 
 
+            total_loss += loss
             ## get the gradients of the loss
             grads = tape.gradient(loss, self.policy.trainable_variables)
 
             ## normalize each gradient
-            grads = [tf.clip_by_norm(grad, 1.0) for grad in grads]
+            #grads = [tf.clip_by_norm(grad, 1.0) for grad in grads]
 
             ## apply the gradients to the policy
             self.policy.optimizer.apply_gradients(zip(grads, self.policy.trainable_variables))
+
+        total_loss = total_loss / len(self.state_memory)
 
         # reset the memory
         self.state_memory = []
         self.reward_memory = []
         self.action_memory = []
+        
+        # return loss
+        return total_loss
 
     ## save the model
     def save_model(self, path):

modelTrain.py
@@ -7,6 +7,7 @@ import numpy as np
 from Agent import PolicyGradientAgent
 import matplotlib.pyplot as plt
 from Environment import DraftEnv
+from scipy.signal import savgol_filter
 
 teams = 12
 rounds = 15
@@ -24,29 +25,21 @@ print(env.observation_space.shape)
 n_actions = env.action_space.n
 count = 0
 
-init_epsilons = [0.8]
-final_epsilons = [0.2]
-layers = [6, 4, 2]
-layer_sizes = [24, 10, 8]
-discount_factors = [0.99, 1]
-learning_rates = [0.05, 0.01, 0.005, 0.001] # 0.01, 0.001, 0.05, 
-
 settings = [
-        [6, 12, 0.005, 0.005, 0.99],
-        [6, 12, 0.005, 0.005, 1],
-        # [2, 10, 0.001, 1],
+        [2, 12, 0.001, 0.001, 0.99, 0.01],
         # [2, 10, 0.005, 1],
         # [6, 10, 0.005, 1]
 ]
 
 for setting in settings:
-        init_epsilon = 0.5
-        final_epsilon = 0.1
+        init_epsilon = 0.0
+        final_epsilon = 0.0
         layers_num = setting[0]
         layer_size = setting[1]
         learning_rate = setting[2]
         final_learning_rate = setting[3]
         discount_factor = setting[4]
+        baseline_rate = setting[5]
 
         agent = PolicyGradientAgent(
                 learning_rate=learning_rate, 
@@ -57,11 +50,15 @@ for setting in settings:
                 layer_size=[layer_size] * layers_num
         )                                       
 
-        episodes = 10000
+        episodes = 1000
         rewards_per_episode = []
         points_per_episode = []
         epsilon_values = []
         roster_per_episode = []
+        loss_per_episode = []
+        actions_per_episode = []
+
+        baseline = 0
 
         for episode in range(1, episodes + 1):
                 state = env.reset()[0]
@@ -69,12 +66,13 @@ for setting in settings:
                 total_reward = 0
                 total_pts = 0
                 roster_slots = 0
-                baseline = 4.847968
+                actions_in_episode = []
                 agent.set_epsilon(init_epsilon - ((episode - 1) / (episodes - 1)) * (init_epsilon - final_epsilon))
                 agent.learning_rate = (learning_rate - ((episode - 1) / (episodes - 1)) * (learning_rate - final_learning_rate))
 
                 while not done:
                         action = agent.choose_action(np.array([state]))
+                        actions_in_episode.append(action)
                         next_state, reward, _ , done , info = env.step(action) # make the step in the environment based on the action
                         agent.store_transition(state, action, reward)
                         total_reward+=reward
@@ -84,16 +82,31 @@ for setting in settings:
                         state = next_state # move to next state
 
                 print('Episode {} finished.'.format(episode))
-                agent.learn(baseline, rounds)
-                rewards_per_episode.append(total_reward-baseline)
-                points_per_episode.append(total_pts)
-                epsilon_values.append(agent.epsilon)
-                roster_per_episode.append(roster_slots)
+
+                loss_per_episode.append(agent.learn(baseline)) ## append the loss per episode (and train the agent)
+                avg_reward = total_reward / rounds ## calculate the average reward
+                rewards_per_episode.append(total_reward-baseline) ## subtract the baseline and append the reward 
+                points_per_episode.append(total_pts) ## append the points
+                epsilon_values.append(agent.epsilon) ## append the epsilon values
+                roster_per_episode.append(roster_slots) ## append the roster slots
+                actions_per_episode.append(actions_in_episode) ## append the actions taken
+
+                ## calculate the new baseline
+                baseline = (1 - baseline_rate) * baseline + baseline_rate * avg_reward
+
+                ## save the model and export the roster
                 agent.save_model(os.path.join(os.getcwd(), 'keras', f'fantasyDrafter_{count}.keras'))
                 env.agentRoster.to_csv(os.path.join(os.getcwd(), 'trainingRosters', 'iteration_' + str(episode) + '_roster.csv'), index=False)
                 
+        percentage = 0.2
+        window_length = max(3, 2 * (int(episodes * percentage) // 2) + 1)  # Ensure integer division and odd window length
+        poly_order = 2
+        smoothed_rewards = savgol_filter(rewards_per_episode, window_length, poly_order)
+        smoothed_points = savgol_filter(points_per_episode, window_length, poly_order)
+        smoothed_loss = savgol_filter(loss_per_episode, window_length, poly_order)
 
-                # Add a text box with the hyperparameters
+
+        # Add a text box with the hyperparameters
         hyperparameters_text = (
                 "Hyperparameters:\n"
                 f"Episodes: {episodes}\n"
@@ -107,7 +120,7 @@ for setting in settings:
 
         # Points and Epsilon Decay Plot
         fig, ax1 = plt.subplots(figsize=(10, 6))
-        ax1.plot(points_per_episode, label='Total Points', color='blue')
+        ax1.plot(smoothed_points, label='Total Points', color='blue')
         ax1.set_xlabel('Episode')
         ax1.set_ylabel('Total Points', color='blue')
         ax1.tick_params(axis='y', labelcolor='blue')
@@ -130,7 +143,7 @@ for setting in settings:
 
         # Rewards and Epsilon Decay Plot
         fig, ax1 = plt.subplots(figsize=(10, 6))
-        ax1.plot(rewards_per_episode, label='Total Rewards', color='blue')
+        ax1.plot(smoothed_rewards, label='Total Rewards', color='blue')
         ax1.set_xlabel('Episode')
         ax1.set_ylabel('Total Rewards', color='blue')
         ax1.tick_params(axis='y', labelcolor='blue')
@@ -177,5 +190,29 @@ for setting in settings:
         # Save the plot to a file
         plt.savefig(os.path.join(os.getcwd(), 'plots', f'{count}_roster.png'), bbox_inches='tight')
         plt.close()  # Close the plot after saving to free memory
+
+        # Plot Average Loss per Episode
+        fig, ax = plt.subplots(figsize=(10, 6))
+        ax.plot(smoothed_loss, label='Average Loss', color='red')
+        ax.set_xlabel('Episode')
+        ax.set_ylabel('Average Loss')
+        ax.set_title('Average Loss per Episode')
+        plt.grid(True)
+        plt.savefig(os.path.join(os.getcwd(), 'plots', f"{count}_avg_loss.png"))
+        plt.close()
+
+        # Flatten the list of actions
+        flat_actions = [action for episode_actions in actions_per_episode for action in episode_actions]
+
+        # Plot a histogram of the actions
+        fig, ax = plt.subplots(figsize=(10, 6))
+        ax.hist(flat_actions, bins=n_actions, color='blue', edgecolor='black')
+        ax.set_xlabel('Action')
+        ax.set_ylabel('Frequency')
+        ax.set_title('Action Distribution Over All Episodes')
+
+        plt.grid(True)
+        plt.savefig(os.path.join(os.getcwd(), 'plots', f"{count}_actions.png"))
+        plt.close()
 
         count+=1